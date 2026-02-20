c20f9db90954484c534a1be3ead1530ec0f2d311
zmedailleu <zmedailleu@users.noreply.github.com>
2026-02-20 21:49:41 +0000

    deploy: 0192e0040d7586ea29b938bfd88514ed47802af4

--- a3-study/vis-data/confidence_interval.R
+++ a3-study/vis-data/confidence_interval.R
@@ -1,16 +1,19 @@
 install.packages(c("jsonlite", "ggplot2"))
 library(jsonlite)
-library(ggplot2)
-
 json_data <- read_json(paste0(getwd(), "/public/a3-study/vis-data/smarty_answers.json"), simplifyVector = TRUE)
 json_df <- as.data.frame(json_data)
+
 colnames(json_df) <- gsub("-", "_", colnames(json_df))
+json_df
+
 json_df$chart_type <- factor(json_df$chart_type, levels = c("pie", "stacked-bar", "bar"))
+
+json_df$guess_percent <- as.numeric(json_df$guess_percent)
 json_df$log_error <- pmax(log2(abs(json_df$guess_percent - json_df$true_percent) + 0.125), 0)
 
+library(ggplot2)
 ggplot(data = json_df, aes(x = log_error, y = chart_type)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
-  stat_summary(fun = mean, geom = "point", size = 5, color = "black") +
-  labs(x = "Log Error", y = "Chart Type")
+  labs(x= "Log Error", y = "Chart Type") +
+  stat_summary(fun.data = mean_sdl, geom = "point", size = 5, color = "black")
 
-ggsave(paste0(getwd(), "/public/a3-study/vis-data/error.png"), width = 8, height = 6, dpi = 300)
